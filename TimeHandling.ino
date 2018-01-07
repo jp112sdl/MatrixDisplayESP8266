801323c360e24317e2f5c86619d887ca9006d05e
jp112sdl <jp112sdl@icloud.com>
2018-01-07 08:56:20 +0100

    fixed time

--- TimeHandling.ino
+++ TimeHandling.ino
@@ -38,7 +38,7 @@ unsigned long sendNTPpacket(IPAddress& address) {
 String calcTime(time_t t) {
   byte Stunde = hour(t) + 1;
   Stunde = (summertime(t, 0)) ? Stunde + 1 : Stunde;
-  if (Stunde == 24) Stunde = 0;
+  if (Stunde > 23) Stunde = Stunde - 24;
   return String((Stunde < 10) ? "0" : "" ) + String(Stunde) +  ":" + String((minute(t) < 10) ? "0" : "" )  + String(minute(t)) + " Uhr";
 }
 
